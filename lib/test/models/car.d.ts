737571fd0097be3d88de3526527d81bac5909df3
DevTesmari <devtest1mari@gmail.com>
2019-05-20 14:38:40 +0900

    dist/arrayprops-alias-combined

---
+++ lib/test/models/car.d.ts
@@ -0,0 +1,9 @@
+import * as mongoose from 'mongoose';
+import { Typegoose } from '../../typegoose';
+export declare class Car extends Typegoose {
+    m: string;
+    version: string;
+    isSedan?: boolean;
+    price: mongoose.Types.Decimal128;
+}
+export declare const model: mongoose.Model<import("../../../../../../../../Users/saferunner/workspace/web/typegoose/src/typegoose").InstanceType<Car>> & Car & typeof Car;
